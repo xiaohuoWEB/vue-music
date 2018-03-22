68e5d56e219d89547318b0a6fe013d16afa40b0b
xiaohuoWEB <xiaohuokj@outlook.com>
2018-03-22 23:48:15 +0800

    搭建项目基本配置和目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,11 +21,13 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'eol-last': 0,
+    'indent': 0,
     'space-before-function-paren': 0
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
-    hello world
+  <div id="app" @touchmove.prevent>
+    <m-header></m-header>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
 
+  export default {
+    components: {
+      MHeader
+    }
+  }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-  #app
-    color: $color-theme
+<style scoped lang="stylus" rel="stylesheet/stylus">
 </style>
