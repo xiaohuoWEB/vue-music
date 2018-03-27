0f019f6aa0adbc5fe28c8555b4ad430241b7f751
霍春阳 <hcy@bizsaas.cn>
2018-03-27 17:38:20 +0800

    封装jsonp 跨域请求 ，推荐页面实现数据获取

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
