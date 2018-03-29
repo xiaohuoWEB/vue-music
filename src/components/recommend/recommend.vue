cd764af47ac4a929132815c5d12c491d1af6f3cd
霍春阳 <hcy@bizsaas.cn>
2018-03-29 17:42:56 +0800

    轮播图功能

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="recommend" ref="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
+        <div class="list-title">热门歌单推荐</div>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -22,9 +36,13 @@
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
