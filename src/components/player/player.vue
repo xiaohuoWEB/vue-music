d3761c8a27842f6c57bb98c306e4576e635298a0
xiaohuoWEB <xiaohuokj@outlook.com>
2018-05-13 00:23:44 +0800

    完善播放器内核完成歌曲进度条功能，接下来实现底部迷你播放器 圆形播放进度条

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -45,7 +45,10 @@
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
-              <progress-bar :percent="percent"></progress-bar>
+              <progress-bar
+                :percent="percent"
+                @percentChange="onProgressBarChange"
+              ></progress-bar>
             </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
@@ -227,6 +230,12 @@
         const minute = interval / 60 | 0
         const second = this._pad(interval % 60)
         return `${minute}:${second}`
+      },
+      onProgressBarChange(percent) {
+        this.$refs.audio.currentTime = this.currentSong.duration * percent
+        if (!this.playing) {
+          this.togglePlaying()
+        }
       },
       _pad(num, n = 2) {
         let len = num.toString().length
